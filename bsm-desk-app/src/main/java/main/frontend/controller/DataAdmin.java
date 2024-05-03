ef2c3d761108bfc7e41ecc46f9ae246efe789225
huyennguyen230103@gmail.com <huyennguyen230103@gmail.com>
2024-05-03 17:20:13 +0700

    User Account screen

--- bsm-desk-app/src/main/java/main/frontend/controller/DataAdmin.java
+++ bsm-desk-app/src/main/java/main/frontend/controller/DataAdmin.java
@@ -3,4 +3,10 @@ package main.frontend.controller;
 public class DataAdmin {
     public static String admin_username;
     public static Integer admin_ID;
+
+    public static String[] role = {"Admin", "Employee"};
+
+    public static String[] status = {"Enable", "Disable"};
+
+
 }
--- bsm-desk-app/src/main/java/main/frontend/controller/updateUserInfoController.java
+++ bsm-desk-app/src/main/java/main/frontend/controller/updateUserInfoController.java
@@ -1,4 +1,5 @@
 package main.frontend.controller;
 
 public class updateUserInfoController {
+
 }
